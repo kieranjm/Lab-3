3320b4aef268b8b76c7b80c2feaeca3d997e416d
kjm61 <kjm61@LAPTOP-90T041MV>
2019-09-26 22:32:53 -0500

    EvenOddCounter is being dumb. It picks one random number and then only uses that number. It won't generate a new number.

---
+++ src/main/java/com/mycompany/lab3/EvenOddCounter.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.lab3;
+
+import java.util.Random;
+
+/**
+ *
+ * @author kjm61
+ */
+public class EvenOddCounter {
+    public static void main(String[] args)
+    {
+        Random rand = new Random();
+        int counter = 100;
+        int odds = 0;
+        int evens = 0;
+        boolean isEven = false;
+        int random = rand.nextInt(1000);
+        
+        for (int i=1; i<=counter; i++)
+        {
+            if (random%2==0)
+            {
+                isEven = true;
+                evens++;
+            }
+            else
+            {
+                isEven = false;
+                odds++;
+            }
+        }
+        System.out.println("There were " + evens + " even numbers.");
+        System.out.println("There were " + odds + " odd numbers.");
+    }
+}
